0ff2a3ce8724f24d1a04c4ade263fd16b2e97386
Renzo Damian <renzo.damian.go@gmail.com>
2024-11-22 18:36:30 +0000

    add files

---
+++ src/ros_robot_controller/ros_robot_controller/ros_robot_controller_sdk/__init__.py
@@ -0,0 +1,4 @@
+from .board import Board
+from .board import PacketReportKeyEvents
+
+__all__ = ['Board', 'PacketReportKeyEvents']
---
+++ src/ros_robot_controller/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+package_name = 'ros_robot_controller'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name, f'{package_name}.ros_robot_controller_sdk'],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch',
+         ['launch/controller.launch.py'] )
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='renzo.damian.go@gmail.com',
+    description='ROS 2 Robot Controller Package',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'controller_node = ros_robot_controller.controller_node:main',
+            'acker_lidar_controller = ros_robot_controller.acker_lidar_node:main',
+        ],
+    },
+)
